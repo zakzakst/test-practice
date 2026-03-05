c570efbdaf3eac2acb86e4d051e0fe3ff26dfc97
zakzakst <e1433104@gmail.com>
2026-03-06 00:56:18 +0900

    fix

--- frontend/components/feature/todo/TodoListItem/index.tsx
+++ frontend/components/feature/todo/TodoListItem/index.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { useState } from "react";
 import type { Todo } from "@/types/todo";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
 
 interface Props extends Omit<Todo, "userId"> {
   onChangeCompleted: (completed: boolean) => {};
@@ -12,9 +16,30 @@ export const TodoListItem = ({
   completed,
   onChangeCompleted,
 }: Props) => {
+  const [editable, setEditable] = useState<boolean>(false);
+
+  const handleEditable = (state: boolean) => {
+    setEditable(state);
+  };
+
+  const handleEdit = () => {};
+
   return (
     <div>
-      <p>{title}</p>
+      <div>
+        <p>{title}</p>
+        <div>
+          <Input />
+        </div>
+      </div>
+      <div>
+        {editable ? (
+          <Button onClick={handleEdit}>決定</Button>
+        ) : (
+          <Button onClick={() => handleEditable(editable)}>編集</Button>
+        )}
+      </div>
+      <Switch />
     </div>
   );
 };
