01e03421e38b95eca2b78c3f11ac7328038141ee
zakzakst <e1433104@gmail.com>
2026-03-30 05:36:56 +0900

    fix

--- frontend/components/common/Pagination/index.tsx
+++ frontend/components/common/Pagination/index.tsx
@@ -1,3 +1,40 @@
 "use client";
 
-const Pagination = () => {};
+import {
+  Pagination as UiPagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+export const Pagination = () => {
+  return (
+    <UiPagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </UiPagination>
+  );
+};
