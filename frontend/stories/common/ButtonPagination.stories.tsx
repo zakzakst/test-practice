2a0b572078eaf3fd04bcb963b28f2bcdae64c683
zakzakst <e1433104@gmail.com>
2026-03-30 17:37:07 +0900

    fix

--- frontend/stories/common/ButtonPagination.stories.tsx
+++ frontend/stories/common/ButtonPagination.stories.tsx
@@ -1,13 +1,21 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import { ButtonPagination } from "@/components/common/ButtonPagination";
+import { fn } from "storybook/test";
 
 const meta = {
   title: "Common/ButtonPagination",
   component: ButtonPagination,
   tags: ["autodocs", "experimental"],
+  args: {
+    onMovePage: fn(),
+  },
 } satisfies Meta<typeof ButtonPagination>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    current: 1,
+  },
+};
