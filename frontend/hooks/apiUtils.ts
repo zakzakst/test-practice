8a660cd7c32381d7c3ffeda0a68bdbc34b57eabc
zakzakst <e1433104@gmail.com>
2026-04-05 15:18:20 +0900

    fix

---
+++ frontend/hooks/apiUtils.ts
@@ -0,0 +1,14 @@
+export const getFetcher = <GetResponse>(url: string) => {
+  return fetch(url).then((res) => res.json() as Promise<GetResponse>);
+};
+
+export const putFetcher = <PutRequest, PutResponse>(
+  url: string,
+  { arg }: { arg: PutRequest },
+) => {
+  return fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(arg),
+  }).then((res) => res.json() as Promise<PutResponse>);
+};
--- frontend/hooks/useTodo4.ts
+++ frontend/hooks/useTodo4.ts
@@ -1,12 +1,22 @@
 import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
 import { Todo } from "@/types/todo";
+import { getFetcher, putFetcher } from "./apiUtils";
 
-const getFetcher = <T>(url: string) => {
-  return fetch(url).then((res) => res.json() as Promise<T>);
-};
-
+// Get
 type GetTodoResponse = Todo;
 
 export const useGetTodo = (id: Todo["id"]) => {
   return useSWR(`/api/todo/${id}`, getFetcher<GetTodoResponse>);
+};
+
+// Put
+type PutTodoRequest = Todo;
+type PutTodoResponse = Todo;
+
+export const usePutTodo = (id: Todo["id"]) => {
+  return useSWRMutation(
+    `/api/todo/${id}`,
+    putFetcher<PutTodoRequest, PutTodoResponse>,
+  );
 };
