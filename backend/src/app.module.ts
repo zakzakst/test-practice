437fb4c1502dec264c7983973daf3f06a06f0c8d
zakzakst <e1433104@gmail.com>
2026-04-07 03:22:41 +0900

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [TodosModule, PostsModule],
 })
 export class AppModule {}
