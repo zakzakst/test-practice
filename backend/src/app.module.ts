57614e0621fa9056a064239946b6fc1ae0d6f0ef
zakzakst <e1433104@gmail.com>
2026-04-08 12:25:06 +0900

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
 import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TodosModule, PostsModule],
+  imports: [TodosModule, PostsModule, UsersModule],
 })
 export class AppModule {}
---
+++ backend/src/users/db.ts
@@ -0,0 +1,14 @@
+import { User } from './entities/user.entity';
+
+export const users: User[] = [
+  {
+    id: 1,
+    email: 'user1@mail.com',
+    passwordHash: 'xxxxx',
+  },
+  {
+    id: 2,
+    email: 'user2@mail.com',
+    passwordHash: 'xxxxx',
+  },
+];
