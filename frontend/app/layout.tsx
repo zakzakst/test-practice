74f98a715287d16d75399f3363fc5b0c4df6b7d7
zakzakst <e1433104@gmail.com>
2026-04-09 04:34:39 +0900

    fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { MSWProvider } from "./msw-provider";
 import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext";
+import { Navbar } from "@/components/common/Navbar";
 
 // TODO: instrumentation.tsに移動する
 if (
@@ -37,7 +39,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MSWProvider>{children}</MSWProvider>
+        <MSWProvider>
+          <AuthProvider>
+            <Navbar />
+            {children}
+          </AuthProvider>
+        </MSWProvider>
       </body>
     </html>
   );
